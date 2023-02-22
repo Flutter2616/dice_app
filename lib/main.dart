e05d484dc58ee92dff3808bcb4ca886514c3a9d9
prince <princerawal225@gmail.com>
2023-02-22 23:12:28 +0530

    otp

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:dice_app/screen/Dice_app.dart';
+import 'package:dice_app/screen/Otp_generetor.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Dice(),
+      home: Otp(),
     ),
   );
 }
